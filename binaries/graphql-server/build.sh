64774e0231863c1043a054ff3a79971ace212fcf
Elvin Luff <elvin@eelviny.me>
2021-12-14 18:49:50 +0100

    fix: podman to docker

--- binaries/graphql-server/build.sh
+++ binaries/graphql-server/build.sh
@@ -2,6 +2,6 @@
 
 LAMBDA_ARCH="linux/arm64" # set this to either linux/arm64 for ARM functions, or linux/amd64 for x86 functions.
 
-podman build . -t localhost/clubclouddemo --platform ${LAMBDA_ARCH}
+docker build . -t localhost/clubclouddemo --platform ${LAMBDA_ARCH}
 mkdir -p lambda/bootstrap
-podman run --platform ${LAMBDA_ARCH} --rm localhost/clubclouddemo > lambda/bootstrap/graphql-server
+docker run --platform ${LAMBDA_ARCH} --rm localhost/clubclouddemo > lambda/bootstrap/graphql-server
